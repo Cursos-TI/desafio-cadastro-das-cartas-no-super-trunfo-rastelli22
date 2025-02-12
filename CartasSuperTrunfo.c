0627e3a32a642fc70a3bfa11eb9355a5c92147ca
rastelli22 <eduardorastelli22@gmail.com>
2025-02-12 22:17:17 +0000

    entrega supertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int codigo;
+    char estado[100];
+    char cidade[100];
+    float populacao;
+    int pt;
+    float pib;
+    float area;
+
+    printf("Insira o codigo da carta:\n");
+    scanf("%d", &codigo);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Insira o nome do estado:\n");
+    scanf("%s", &estado);
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Insira o nome da cidade:\n");
+    scanf("%s", &cidade);
+    
+    printf("Insira a populacao:\n");
+    scanf("%f", &populacao);
+    
+    printf("Insira o numero de pontos turisticos:\n");
+    scanf("%d", &pt);
+    
+    printf("Insira o pib:\n");
+    scanf("%f", &pib);
+    
+    printf("Insira a area:\n");
+    scanf("%f", &area);
+
+    printf("Código: %d\n", codigo);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %f\n", populacao);
+    printf("Pontos turisticos: %d\n", pt);
+    printf("Pib: %f\n", pib);
+    printf("Area: %f\n", area);
 
     return 0;
+
+
+
+    
+
+
+
+
+
+
 }
